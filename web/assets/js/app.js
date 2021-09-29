7ea9e533b62616b1b7fa5d7fb078a3d74cad5c06
higorch <higorchristianfe@gmail.com>
2021-09-29 11:06:41 -0300

    frontend

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,5 +1,23 @@
 (function ($) {
 
+    var symptoms = [];
 
+    $(document).on('click', '.sidebar-symptoms .box ul li label', function (e) {
+
+        e.preventDefault();
+
+        var el = $(this);
+        var symptom = el.find('input[type="checkbox"]').data('symptom');
+
+        if (symptoms.length >= 0 && symptoms.indexOf(symptom) == -1) {
+            el.parent('li').addClass('active');
+            symptoms.push(symptom);
+        } else {
+            el.parent('li').removeClass('active');
+            symptoms.pop(symptom);
+        }
+
+        console.log(symptoms);
+    });
 
 })(jQuery);
